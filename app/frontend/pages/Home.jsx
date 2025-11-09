da7e88467ad89b7d741ffc542c3e5b440a12e31f
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-11-09 14:38:44 -0300

    Implement Home page with Inertia and update application layout

---
+++ app/frontend/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Olá, tudo bem?</h2>
+        <p>Bem-vindo ao Tasklamation!</p>
+    </div>
+  )
+}
+
+export default Home
