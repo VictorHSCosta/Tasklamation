68cf7d58c67454073d23822b0bfb0b7a0f0b54e7
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-11-13 19:50:20 -0300

    Refactor inertia_share to include flash messages and current user details

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
  
-  inertia_share user: -> { current_user }
-
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
-
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  inertia_share do
+    {
+      flash: {
+        notice: flash.notice,
+        alert:  flash.alert
+      },
+      current_user: current_user&.slice(:id, :email)
+    }
+  end
 end
