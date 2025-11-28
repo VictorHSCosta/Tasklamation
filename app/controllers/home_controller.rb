eb253aa2d6fe590f1d91dc7cda5133062b0aaecf
VictorHSCosta <victorhenriquecosta23@gmail.com>
2025-11-27 23:03:09 -0300

    Add user authentication with Devise and implement login/register pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
     def index
-        render inertia: "Home" ,props: {
+        render inertia: "Home", props: {
             welcomeMessage: "Welcome to the Home Page!"
         }
     end
--- app/frontend/pages/Home.jsx
+++ app/frontend/pages/Home.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import { router } from '@inertiajs/react'
 
 const Home = () => {
   return (
     <div>
       <h2>Olá, tudo bem?</h2>
         <p>Bem-vindo ao Tasklamation!</p>
+        <button onClick={() => {router.delete('/users/sign_out', {preserveScroll: true})}}>
+          Ir para Login
+        </button>
     </div>
   )
 }
